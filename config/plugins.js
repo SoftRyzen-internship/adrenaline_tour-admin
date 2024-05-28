457b731f5630fe505537e7c15220b2c54906858c
Svitlana Zhovanik <lanasvetcat@gmail.com>
2024-05-28 13:10:00 +0300

    add plugin graphql

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,14 @@
-module.exports = () => ({});
+module.exports = ({env}) => ({
+    graphql: {
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+});
