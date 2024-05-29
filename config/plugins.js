61a0fb8e876545226a344d47e42d1eb1de9f8cf3
Andrii Gadar <gadar.andre@gmail.com>
2024-05-30 00:21:06 +0300

    added cloudinary settings

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,28 @@
-module.exports = ({env}) => ({
-    graphql: {
+module.exports = ({ env }) => ({
+  graphql: {
     config: {
-      endpoint: '/graphql',
+      endpoint: "/graphql",
       shadowCRUD: true,
       playgroundAlways: false,
       depthLimit: 7,
       amountLimit: 100,
       apolloServer: {
         tracing: false,
+      },
+    },
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
   },
