e2e38db00003b8d0aa16ca0020f7b04af4396503
Andrii Gadar <gadar.andre@gmail.com>
2024-06-12 11:24:41 +0300

    add deploy config

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,15 @@
 module.exports = ({ env }) => ({
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook: env("VERCEL_DEPLOY_PLUGIN_HOOK"),
+      apiToken: env("VERCEL_DEPLOY_PLUGIN_API_TOKEN"),
+      appFilter: env("VERCEL_DEPLOY_PLUGIN_APP_FILTER"),
+      teamFilter: env("VERCEL_DEPLOY_PLUGIN_TEAM_FILTER"),
+      roles: ["strapi-super-admin"],
+    },
+  },
+
   graphql: {
     config: {
       endpoint: "/graphql",
@@ -11,6 +22,7 @@ module.exports = ({ env }) => ({
       },
     },
   },
+
   upload: {
     config: {
       provider: "cloudinary",
